fcf8f10434fedcb12a21aecd9ca1065495b2e135
obulyk <bulyk.oksana@gmail.com>
2022-02-07 03:01:33 +0200

    Commit script to git repository and provide link as home task result.

--- secondTask.py
+++ secondTask.py
@@ -1,17 +1,33 @@
+from collections import defaultdict
 import random
 import string
-
-[{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
+# empty list
+l = []
+# random number from 2 to 10
 randomNumberOfList = random.randint(2, 10)
-lst = []
+#cycle from 0 to random number
+for i in range(randomNumberOfList):
+# crate a dictionary with random letter and random int from 0 to 100
+    tple = {random.choice(string.ascii_lowercase): random.randint(0, 100),
+            random.choice(string.ascii_lowercase): random.randint(0, 100),
+            random.choice(string.ascii_lowercase): random.randint(0, 100)}
+# add each created dict into list
+    l.append(tple)
+print(l)
+# maybe I will need that list in a future
+commonDictionary = {}
 
-for i in range(2):
-    value = random.randint(0, 10)
-    randomLetter = random.choice(string.ascii_lowercase)
-    tple = {random.choice(string.ascii_lowercase): random.randint(0, 10),
-            random.choice(string.ascii_lowercase): random.randint(0, 10),
-            random.choice(string.ascii_lowercase): random.randint(0, 10)}
-    lst.append(tple)
-print(lst)
+# defaultdict used to make default empty list
+res = defaultdict(list)
+#each dictionary {} in List
+for sub in l:
+# Each key in dictionary
+    for key in sub:
+# result with key(for ex: q) add key from dictionary {'l': 50, 'e': 84, 'y': 98}
+        res[key].append(sub[key])
+#returns the value of the associated key
+        res.get(key)
+# printing result
+print(dict(res))
 
 
